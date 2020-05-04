ce27227baa881e1f2b084364be1ec421d1250f22
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2020-05-04 14:47:02 +0200

    :bug: Introducing rewriting for toutatice CAS

--- src/cas/toutatice.js
+++ src/cas/toutatice.js
@@ -1,8 +1,7 @@
 const jsdom = require('jsdom');
 const util = require('../util');
 
-async function login({ username, password, url })
-{
+async function login({ username, password, url }) {
     console.log(`Logging in '${username}' for '${url}' using Toutatice CAS`);
 
     let jar = new jsdom.CookieJar();
@@ -11,34 +10,73 @@ async function login({ username, password, url })
         jar
     });
 
-    let params = util.getParams(dom);
-    params['_saml_idp'] = 'eleve-1';
+    // let params = util.getParams(dom);
+    // params['_saml_idp'] = 'eleve-1';
+
 
     dom = await util.getDOM({
-        url: 'https://www.toutatice.fr/wayf/Ctrl',
-        jar,
-        method: 'POST',
-        data: params
+        url: dom.window.document.getElementById('img-responsive'),
+        jar
     });
 
-    params = util.getParams(dom);
-    params['j_username'] = username;
-    params['j_password'] = password;
-
-    dom = await util.getDOM({
-        url: 'https://www.toutatice.fr' + dom.window.document.getElementsByTagName('form')[0].action,
-        jar,
-        method: 'POST',
-        data: params
-    });
+    dom.window.document.getElementById('username').value = username;
+    dom.window.document.getElementById('password').value = password;
 
     dom = await util.submitForm({
         dom,
         jar,
-        asIs: true
+        actionRoot: 'https://educonnect.education.gouv.fr/',
+        extraParams: {
+            '_eventId_proceed': ''
+        }
     });
 
-    return util.tryExtractStart(username, dom);
+    if (!dom.window.document.querySelector('input[name=SAMLResponse]')) {
+        console.log(`Wrong IDs for '${username}'`);
+        throw 'Mauvais identifiants';
+    }
+
+    await util.submitForm({
+        dom,
+        jar
+    });
+
+    console.log(`Logged in '${username}'`);
+
+    return util.extractStart(await util.getDOM({
+        url,
+        jar,
+        asIs: true
+    }));
 }
 
 module.exports = login;
+
+// dom = await util.getDOM({
+//     url: 'https://www.toutatice.fr/wayf/Ctrl',
+//     jar,
+//     method: 'POST',
+//     data: params
+// });
+
+// params = util.getParams(dom);
+// params['j_username'] = username;
+// params['j_password'] = password;
+
+// dom = await util.getDOM({
+//     url: 'https://www.toutatice.fr' + dom.window.document.getElementsByTagName('form')[0].action,
+//     jar,
+//     method: 'POST',
+//     data: params
+// });
+
+// dom = await util.submitForm({
+//     dom,
+//     jar,
+//     asIs: true
+// });
+
+// return util.tryExtractStart(username, dom);
+// }
+
+// module.exports = login;
