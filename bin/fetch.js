ef1d118602c4b3709945cccd3cdfd717eaa89753
Thibaut <30575266+trmaud@users.noreply.github.com>
2020-08-16 03:34:10 +0200

    Debug 3

--- bin/fetch.js
+++ bin/fetch.js
@@ -21,9 +21,9 @@ async function fetch()
     const marks = await session.marks();
     const evaluations = await session.evaluations();
     const absences = await session.absences();
-    const infos = await  session.infos();
+    const infos = await session.infos();
     const homeworks = await session.homeworks(new Date(2020, 8, 15));
-  
+
     // TODO
 
     console.log('Done');
