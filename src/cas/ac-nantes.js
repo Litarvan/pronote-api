7a40efca85b418506b03aa59f90855002bd52f05
Adrien Navratil <adrien1975@live.fr>
2020-09-04 20:39:29 +0200

    Some CAS generification

--- src/cas/ac-nantes.js
+++ src/cas/ac-nantes.js
@@ -1,37 +1,16 @@
-const jsdom = require('jsdom');
+const wayf = require('./generics/wayf');
 
-const { getParams, getDOM, extractStart } = require('./api');
-const aten = require('./generics/aten');
+module.exports = (url, account, username, password) => wayf({
+    url,
+    username,
+    password,
 
-async function login(url, account, username, password)
-{
-    const jar = new jsdom.CookieJar();
+    startURL: 'https://cas3.e-lyco.fr/access/',
+    wayfURL: 'https://cas3.e-lyco.fr/discovery/',
+    atenURL: 'https://ats-idp.ac-nantes.fr/',
 
-    let dom = await getDOM({
-        url: `https://cas3.e-lyco.fr/access/login?service=${encodeURIComponent(url)}`,
-        jar
-    });
-
-    const params = getParams(dom);
-    params.origin = 'https://ats-idp.ac-nantes.fr/SAML/FIM';
-    params.action = 'selection';
-    dom = await getDOM({
-        url: 'https://cas3.e-lyco.fr/discovery/WAYF',
-        jar,
-        data: params,
-        runScripts: true,
-        hook: aten.hook
-    });
-
-    dom = await aten.submit({
-        dom,
-        jar,
-        username,
-        password,
-        atenURL: 'https://ats-idp.ac-nantes.fr/login/'
-    });
-
-    return extractStart(dom);
-}
-
-module.exports = login;
+    extraParams: {
+        origin: 'https://ats-idp.ac-nantes.fr/SAML/FIM',
+        action: 'selection'
+    }
+});
