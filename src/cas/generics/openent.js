88bf3195b469e630064f24f669398a1ce4f93eb1
Adrien Navratil <adrien1975@live.fr>
2020-09-10 05:40:35 +0200

    Fixed some OpenENT issues

--- src/cas/generics/openent.js
+++ src/cas/generics/openent.js
@@ -1,9 +1,17 @@
 const jsdom = require('jsdom');
 
+const http = require('../../http');
 const { getDOM, extractStart } = require('../api');
 
 async function login({ url, username, password, target })
 {
+    const location = await http({ url, followRedirects: 'get' });
+
+    let service = encodeURIComponent(url);
+    if (location.startsWith('http') && location.includes('service=')) {
+        service = location.substring(location.indexOf('=') + 1);
+    }
+
     return extractStart(await getDOM({
         url: `https://${target}/auth/login`,
         jar: new jsdom.CookieJar(),
@@ -11,7 +19,7 @@ async function login({ url, username, password, target })
         data: {
             email: username,
             password,
-            callback: '/cas/login?service=' + encodeURIComponent(url)
+            callback: `/cas/login?service=${service}`
         },
         asIs: true
     }));
