181d0cf1f8a14d52269540ea645794db6277cc09
Androz2091 <androz2091@gmail.com>
2020-09-14 18:38:12 +0200

    eslint fixes

--- src/geo.js
+++ src/geo.js
@@ -4,7 +4,6 @@ function geo(lat, long) {
     return new Promise((resolve, reject) => {
 
         const data = `data={"nomFonction": "geoLoc","lat": ${lat.toString()},"long": ${long.toString()}}`;
-        console.log(data)
 
         const options = {
             hostname: 'www.index-education.com',
@@ -13,17 +12,18 @@ function geo(lat, long) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                // eslint-disable-next-line max-len
                 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
                 'Content-Length': data.length
             }
         }
 
         let incomingData = '';
-        const request = https.request(options, function (res) {
-            res.on('data', function (data) {
+        const request = https.request(options, res => {
+            res.on('data', data => {
                 incomingData += data;
             });
-            res.on('end', function () {
+            res.on('end', () => {
                 resolve(JSON.parse(incomingData));
             });
         });
@@ -31,7 +31,7 @@ function geo(lat, long) {
         request.write(data);
         request.end();
 
-        request.on('error', (error) => {
+        request.on('error', error => {
             reject(error);
         });
     })
